be679462d81e7c1cdd5def38a96da78eaa2bce05
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-08-15 18:45:53 -0300

    criado e implementado fucano de remover tarefa

--- controllers/gerenciador-tarefas.js
+++ controllers/gerenciador-tarefas.js
@@ -92,7 +92,15 @@ function atualizarTarefa(req, res) {
     });
 }
 
-function removerTarefa(req, res) {}
+function removerTarefa(req, res) {
+    const id = req.params.id;
+    const numTarefas = tarefas.length;
+    tarefas = tarefas.filter((tarefa) => tarefa.id !== id);
+    if (numTarefas === tarefas.length) {
+        res.status(404).json({ erro: "Tarefa não encontrada" });
+    }
+    res.json({ msg: "Tarefa removida com sucesso" });
+}
 
 function concluirTarefa(req, res) {}
 
